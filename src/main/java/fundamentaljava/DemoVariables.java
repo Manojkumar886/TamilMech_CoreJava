8181629870f17a2dfed466119fea8f74ce96bb85
Manojkumar886 <maddymano434@gmail.com>
2023-11-03 12:00:17 +0530

    Local and Instance variable is over

---
+++ src/main/java/fundamentaljava/DemoVariables.java
@@ -0,0 +1,34 @@
+package fundamentaljava;
+
+import java.util.Scanner;
+// java-->util--->Scanner--->nextInt()
+//java---->fundamentaljava--->Demovariable--->local()
+public class DemoVariables
+{
+
+    public void Local()
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("MY College Percentage ");
+        float Percentage=scan.nextFloat();//Local Variable
+        System.out.println("my percentage :"+Percentage);
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner scan=new Scanner(System.in);//object
+
+        System.out.println(" Tell us your10th Mark");
+        int mark_10th=scan.nextInt();//Instance Variable
+
+        String RegisterName; //Instance variable
+
+        System.out.println("my 10th total mark is :"+mark_10th);
+
+        DemoVariables demo=new DemoVariables();//Current class object
+        demo.Local();//Activated your method
+
+        System.out.println("Thank you");
+    }   
+    
+}
