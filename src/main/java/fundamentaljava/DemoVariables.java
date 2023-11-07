8f67751ab444067875527a7ae76796f61675fb56
Manojkumar886 <maddymano434@gmail.com>
2023-11-07 11:47:57 +0530

    Global and Static variable is over

--- src/main/java/fundamentaljava/DemoVariables.java
+++ src/main/java/fundamentaljava/DemoVariables.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 //java---->fundamentaljava--->Demovariable--->local()
 public class DemoVariables
 {
+ 
 
     public void Local()//user defined method
     {
+        // DemoVariables demo1=new DemoVariables();
+        // demo1.Local();
         Scanner scan=new Scanner(System.in);
         System.out.println("MY College Percentage ");
         float Percentage=scan.nextFloat();//Local Variable
+        System.out.println("my college name");
+        String college_name=scan.next();
         System.out.println("my percentage :"+Percentage);
+        System.out.println("my college nmse:"+college_name);
+        System.out.println("My Global Value is :"+Alpha);//accessed to global variable;
+        System.out.println("my static variable using method :"+Initial);//accessed to static variable
     }
 
     public static void main(String[] args) //main method
@@ -29,6 +37,12 @@ public class DemoVariables
         demo.Local();//Activated your method
 
         System.out.println("Thank you");
-    }   
+
+       
+        System.out.println("My Global value with using object: "+demo.Alpha );//Accessed global variable with using object 
+        System.out.println("my static variable is initial :"+Initial);//easy to access static
+    }  
+    int Alpha=908; //global variable
+    static char Initial='M';//static variable    
     
 }
